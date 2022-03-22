f8b15854ce339f58fd4bc48ecd2ef0293bff5137
nikeshhbaskaran01@gmail.com <nikeshhbaskaran01@gmail.com>
2022-03-22 13:51:18 -0400

    Added API connectivity changes

--- Miscellaneous/sample-app-aws-amplify-ssl-test/src/App.js
+++ Miscellaneous/sample-app-aws-amplify-ssl-test/src/App.js
@@ -1,22 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
+import { BASE_URL } from "./base.js";
 
 function App() {
+
+  const connectToTheBackend = async () => {
+    const config = {
+        headers: {
+            "Content-Type": "application/json",
+        }
+    };
+
+    const { data } = await axios.get(
+        BASE_URL + `/hello`,
+        config
+    );
+
+    alert(data);
+  }
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={connectToTheBackend}>Connect to the backend</button>
       </header>
     </div>
   );
